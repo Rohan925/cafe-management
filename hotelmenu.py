a97bacf258d56419a835bf33b5837273c880ee1a
ROHAN <132924993+Rohan925@users.noreply.github.com>
2025-08-02 15:57:21 +0530

    Add hotel menu ordering system

---
+++ hotelmenu.py
@@ -0,0 +1,33 @@
+menu = {
+    'Pizza': 40,
+    'Pasta': 50,
+    'Burger': 60,
+    'Salad': 70,
+    'Coffee': 30,
+}
+
+print("Welcome to MY Restaurant")
+print("Menu:")
+for item, price in menu.items():
+    print(f"{item}: Rs{price}")
+
+order_total = 0
+
+item_1 = input("Enter the name of the item you want to order: ")
+if item_1 in menu:
+    order_total += menu[item_1]
+    print(f"Your item '{item_1}' has been added to your order.")
+else:
+    print(f"Ordered item '{item_1}' is not available yet!")
+
+another_item = input("Do you want to add another item? (Yes/No): ")
+
+if another_item.lower() == "yes":
+    item_2 = input("Enter the name of the second item: ")
+    if item_2 in menu:
+        order_total += menu[item_2]
+        print(f"Your item '{item_2}' has been added to your order.")
+    else:
+        print(f"Ordered item '{item_2}' is not available!")
+        
+print(f"The total amount to pay is: Rs{order_total}")
